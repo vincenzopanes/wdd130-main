6bbb148b0541e878d10fd624243707968cf2fcde
vincenzopanes <vpanesirribarra@byupathway.edu>
2026-03-17 21:21:11 -0300

    Code-along Activity w02 changes

---
+++ CSE111/ad_numbers.py
@@ -0,0 +1,15 @@
+def add(firts, second):
+    """
+    Add two numbers together and returns the results.
+    """
+    result = firts + second
+    return result
+
+
+x = 3
+y = 4
+total = add(x, y)
+print(total)
+
+total = add(y, y)
+print(total)
---
+++ CSE111/functions.py
@@ -0,0 +1,5 @@
+def welcome (name):
+    """Pruprose: Display a Welcome message"""
+    print(f"Hello {name}! Welcome to the function factory.")
+user = input("What is your name? ")
+welcome(user)
